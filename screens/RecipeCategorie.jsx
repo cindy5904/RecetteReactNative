1678928455bba31433672b976d9c552229fc94ed
Cindy Telmon <telcindy59@gmail.com>
2024-03-15 11:51:57 +0100

    ajout icone

--- screens/RecipeCategorie.jsx
+++ screens/RecipeCategorie.jsx
@@ -1,97 +1,145 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, Pressable } from 'react-native'
-import React from 'react'
-import { MEALS } from '../recettes/data/data.js'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  Pressable,
+} from 'react-native';
+import React from 'react';
+import {MEALS} from '../recettes/data/data.js';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function RecipeCategorie({navigation, route}) {
-  const category  = route.params.category;
-  const categoryMeals = MEALS.filter((meal) => meal.categoryIds.includes(category.id));
-  
+  const category = route.params.category;
+  const categoryMeals = MEALS.filter(meal =>
+    meal.categoryIds.includes(category.id),
+  );
+
   return (
     <View style={styles.container}>
-    <ScrollView>   
-    <Text style={styles.title}>Recettes de la catégorie <Text style={styles.categori}>{category.title}</Text></Text>
-    <View style={styles.mealsContainer}>
-      {categoryMeals.map((meal) => (
-        <TouchableOpacity key={meal.id} style={styles.card} onPress={() => { navigation.navigate('Detail Recipe', { meal: meal })}} activeOpacity={0.7}>
-          <Image style={styles.img} source={{ uri: meal.imageUrl }} />
-          <View style={styles.cardContent}>
-            <Text style={styles.mealTitle}>{meal.title}</Text>
-            <View style={styles.textContent}>
-              <Text style={styles.text}>{meal.duration} mn</Text>
-              <Text style={styles.text}>{meal.complexity.toUpperCase()}</Text>
-              <Text style={styles.text}>{meal.affordability.toUpperCase()}</Text>
-            </View>
-          </View>
-        </TouchableOpacity>
-      ))}
+      <ScrollView>
+        <Text style={styles.title}>
+          Recettes de la catégorie{' '}
+          <Text style={styles.categori}>{category.title}</Text>
+        </Text>
+        <View style={styles.mealsContainer}>
+          {categoryMeals.map(meal => (
+            <TouchableOpacity
+              key={meal.id}
+              style={styles.card}
+              onPress={() => {
+                navigation.navigate('Detail Recipe', {meal: meal});
+              }}
+              activeOpacity={0.7}>
+              <Image style={styles.img} source={{uri: meal.imageUrl}} />
+              <View style={styles.cardContent}>
+                <Text style={styles.mealTitle}>{meal.title}</Text>
+                <View style={styles.textContent}>
+                  <Text style={styles.text}>{meal.duration} mn</Text>
+                  <Text style={styles.text}>
+                    {meal.complexity.toUpperCase()}
+                  </Text>
+                  <Text style={styles.text}>
+                    {meal.affordability.toUpperCase()}
+                  </Text>
+                </View>
+                <View style={styles.iconContainer}>
+                  {meal.isVegan && (
+                  <Image style={styles.imgIcone} source={require('../asset/vegan.png')} />)}
+                  {meal.isGlutenFree && (
+                    <Image style={styles.imgIconeGluten} source={require('../asset/gluten.png')} />)}
+                  {meal.isVegetarian && (
+                    <Image style={styles.imgIcone} source={require('../asset/vegan.png')} />)}
+                  {meal.isLactoseFree && (
+                    <Image style={styles.imgIcone} source={require('../asset/lactose.jpg')} />                  )}
+                </View>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </ScrollView>
     </View>
-    </ScrollView> 
-  </View>
-);
+  );
 }
 
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  alignItems: 'center',
-},
-title: {
-  fontSize: 22,
-  fontWeight: 'bold',
-  marginBottom: 20,
-  marginTop: 15,
-  textAlign: 'center',
-},
-categori: {
-    color:'#004d51',
-},
-mealsContainer: {
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    marginTop: 15,
+    textAlign: 'center',
+  },
+  categori: {
+    color: '#004d51',
+  },
+  mealsContainer: {
     width: '100%',
     paddingHorizontal: 20,
-    justifyContent: 'center', 
+    justifyContent: 'center',
     alignItems: 'center',
     margin: 'auto',
-  
-},
-card: {
-  backgroundColor: '#fff',
-  width: 370,
-  height: 320,
-  marginBottom: 38,
-  borderRadius: 10,
-  shadowColor: '#000',
-  shadowOffset: { width: 0, height: 2 },
-  shadowOpacity: 0.25,
-  shadowRadius: 3.84,
-  elevation: 5,
-},
-img: {
-  width: '100%',
-  height: 230,
-  borderTopLeftRadius: 10,
-  borderTopRightRadius: 10,
-},
-cardContent: {
-  padding: 5,
-},
-mealTitle: {
-  textAlign: 'center',
-  fontSize: 22,
-  fontWeight: 'bold',
-  
-},
-textContent: {
-  flexDirection: 'row',
-  gap: 15,
-  fontSize: 18,
-  textAlign: 'center',
-  justifyContent: 'center',
-  marginTop: 10,
-  color: 'black',
-  
-},
-text: {
+  },
+  card: {
+    backgroundColor: '#fff',
+    width: 370,
+    height: 380,
+    marginBottom: 38,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  img: {
+    width: '100%',
+    height: 230,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  cardContent: {
+    padding: 5,
+  },
+  mealTitle: {
+    textAlign: 'center',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  textContent: {
+    flexDirection: 'row',
+    gap: 15,
+    fontSize: 18,
+    textAlign: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+    color: 'black',
+  },
+  text: {
     color: 'black',
     fontSize: 15,
-}
-})
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    textAlign: 'center',
+    marginTop: 15,
+    gap : 10,
+    
+  },
+  imgIcone: {
+    width: 40,
+    height: 40,
+  }, 
+  imgIconeGluten: {
+    width: 25,
+    height: 25,
+    marginTop: 8
+  }
+});
